3f5a84c058ad60e439af985614fe1dd0c006bfd0
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-08-11 19:22:43 -0300

    first commit

---
+++ LFSDKTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LFSDKTest
+//
+//  Created by Luiz Fernando dos Santos on 11/08/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
